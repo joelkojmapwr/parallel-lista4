29507a1b3d5973164c13ed11b570c995fd12a23d
279745@student.pwr.edu.pl <joel.kojma.pwrrt@gmail.com>
2025-06-15 12:44:39 +0200

    Condition logic implemented, but needs testing

--- zad4/monitor.go
+++ zad4/monitor.go
@@ -4,10 +4,14 @@ type responseChannel struct {
 	resp chan struct{}
 }
 
+type boolResponseChannel struct {
+	resp chan bool
+}
+
 type Monitor struct {
-	enterChannel     <-chan responseChannel
-	leaveChannel     <-chan responseChannel
-	terminateChannel <-chan struct{}
+	enterChannel     chan responseChannel
+	leaveChannel     chan responseChannel
+	terminateChannel chan struct{}
 }
 
 func (m *Monitor) run() {
@@ -28,10 +32,11 @@ func (m *Monitor) run() {
 }
 
 type Condition struct {
-	myCount        int
-	signalChannel  chan responseChannel
-	preWaitChannel chan responseChannel
-	waitChannel    chan responseChannel
+	myCount          int
+	signalChannel    chan responseChannel
+	preWaitChannel   chan responseChannel
+	waitChannel      chan responseChannel
+	isWaitingChannel chan boolResponseChannel
 }
 
 func createCondition() *Condition {
@@ -44,5 +49,36 @@ func createCondition() *Condition {
 }
 
 func (c *Condition) run(monitor *Monitor) {
+	for {
+		var monitorResponseChannel = make(chan struct{})
+		if c.myCount == 0 {
+			select {
+			case msg := <-c.signalChannel:
 
+				monitor.leaveChannel <- responseChannel{resp: monitorResponseChannel}
+				<-monitorResponseChannel
+				msg.resp <- struct{}{}
+
+			case msg := <-c.preWaitChannel:
+				c.myCount++
+				msg.resp <- struct{}{}
+
+			case msg := <-c.waitChannel:
+				msg.resp <- struct{}{}
+			waitLoop:
+				for {
+					select {
+					case msg := <-c.signalChannel:
+						c.myCount--
+						msg.resp <- struct{}{}
+						break waitLoop
+
+					case msg := <-c.preWaitChannel:
+						c.myCount++
+						msg.resp <- struct{}{}
+					}
+				}
+			}
+		}
+	}
 }
