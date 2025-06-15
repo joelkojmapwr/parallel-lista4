f0b67f0e28ec8c29ef18da693312ecb0b48c6a8a
279745@student.pwr.edu.pl <joel.kojma.pwrrt@gmail.com>
2025-06-15 13:15:13 +0200

    created Read Write Monitor but also needs testing

--- zad4/monitor.go
+++ zad4/monitor.go
@@ -14,6 +14,14 @@ type Monitor struct {
 	terminateChannel chan struct{}
 }
 
+func createMonitor() *Monitor {
+	return &Monitor{
+		enterChannel:     make(chan responseChannel),
+		leaveChannel:     make(chan responseChannel),
+		terminateChannel: make(chan struct{}),
+	}
+}
+
 func (m *Monitor) run() {
 	for {
 		select {
@@ -38,9 +46,10 @@ type Condition struct {
 	waitChannel      chan responseChannel
 	isWaitingChannel chan boolResponseChannel
 	terminateChannel chan struct{}
+	monitor          *Monitor
 }
 
-func createCondition() *Condition {
+func createCondition(monitor *Monitor) *Condition {
 	return &Condition{
 		myCount:          0,
 		signalChannel:    make(chan responseChannel),
@@ -48,17 +57,18 @@ func createCondition() *Condition {
 		waitChannel:      make(chan responseChannel),
 		isWaitingChannel: make(chan boolResponseChannel),
 		terminateChannel: make(chan struct{}),
+		monitor:          monitor,
 	}
 }
 
-func (c *Condition) run(monitor *Monitor) {
+func (c *Condition) run() {
 	for {
 		var monitorResponseChannel = make(chan struct{})
 		if c.myCount == 0 {
 			select {
 			case msg := <-c.signalChannel:
 
-				monitor.leaveChannel <- responseChannel{resp: monitorResponseChannel}
+				c.monitor.leaveChannel <- responseChannel{resp: monitorResponseChannel}
 				<-monitorResponseChannel
 				msg.resp <- struct{}{}
 
@@ -124,4 +134,34 @@ func (c *Condition) run(monitor *Monitor) {
 			}
 		}
 	}
+}
+
+func nonEmpty(c *Condition) bool {
+	var respChannel = boolResponseChannel{resp: make(chan bool)}
+	c.isWaitingChannel <- respChannel
+	return <-respChannel.resp
+}
+
+func (c *Condition) signal() {
+	var respChannel = responseChannel{resp: make(chan struct{})}
+	c.signalChannel <- respChannel
+	<-respChannel.resp
+}
+func (c *Condition) preWait() {
+	var respChannel = responseChannel{resp: make(chan struct{})}
+	c.preWaitChannel <- respChannel
+	<-respChannel.resp
+}
+func (c *Condition) wait() {
+	var respChannel = responseChannel{resp: make(chan struct{})}
+	c.waitChannel <- respChannel
+	<-respChannel.resp
+}
+
+func (c *Condition) publicWait() {
+	c.preWait()
+	var respChannel = make(chan struct{})
+	c.monitor.leaveChannel <- responseChannel{resp: respChannel}
+	<-respChannel
+	c.wait()
 }
---
+++ zad4/rwMonitor.go
@@ -0,0 +1,84 @@
+package main
+
+type RWMonitor struct {
+	OK_to_Read       *Condition
+	OK_to_Write      *Condition
+	writing          bool
+	terminateChannel chan struct{}
+	readers          int
+	monitor          *Monitor
+}
+
+func createRWMonitor() *RWMonitor {
+	monitor1 := createMonitor()
+	return &RWMonitor{
+		OK_to_Read:       createCondition(monitor1),
+		OK_to_Write:      createCondition(monitor1),
+		writing:          false,
+		readers:          0,
+		terminateChannel: make(chan struct{}),
+		monitor:          monitor1,
+	}
+}
+
+func (rw *RWMonitor) run() {
+	rw.monitor.run()
+	rw.OK_to_Read.run()
+	rw.OK_to_Write.run()
+
+}
+
+func (rw *RWMonitor) terminate() {
+	rw.monitor.terminateChannel <- struct{}{}
+	rw.OK_to_Read.terminateChannel <- struct{}{}
+	rw.OK_to_Write.terminateChannel <- struct{}{}
+}
+
+func (rw *RWMonitor) startRead() {
+	respChan := make(chan struct{})
+	rw.monitor.enterChannel <- responseChannel{resp: respChan}
+	<-respChan
+	if rw.writing || nonEmpty(rw.OK_to_Write) {
+		rw.OK_to_Read.publicWait()
+	}
+	rw.readers++
+	rw.OK_to_Read.signal()
+}
+
+func (rw *RWMonitor) stopRead() {
+	respChan := make(chan struct{})
+	rw.monitor.enterChannel <- responseChannel{resp: respChan}
+	<-respChan
+	rw.readers--
+	if rw.readers == 0 {
+		rw.OK_to_Write.signal()
+	} else {
+		rw.monitor.leaveChannel <- responseChannel{resp: respChan}
+		<-respChan
+	}
+
+}
+
+func (rw *RWMonitor) startWrite() {
+	respChan := make(chan struct{})
+	rw.monitor.enterChannel <- responseChannel{resp: respChan}
+	<-respChan
+	if rw.writing || rw.readers > 0 {
+		rw.OK_to_Write.publicWait()
+	}
+	rw.writing = true
+	rw.monitor.leaveChannel <- responseChannel{resp: respChan}
+	<-respChan
+}
+
+func (rw *RWMonitor) stopWrite() {
+	respChan := make(chan struct{})
+	rw.monitor.enterChannel <- responseChannel{resp: respChan}
+	<-respChan
+	rw.writing = false
+	if nonEmpty(rw.OK_to_Read) {
+		rw.OK_to_Read.signal()
+	} else {
+		rw.OK_to_Write.signal()
+	}
+}
